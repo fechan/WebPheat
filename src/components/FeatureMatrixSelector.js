b5d1a6fc6fd514fa77e0015ed1e2e006eec47d8a
fechan <work.fechan@gmail.com>
2024-01-11 23:47:24 -0800

    handle all cases

--- src/components/FeatureMatrixSelector.js
+++ src/components/FeatureMatrixSelector.js
@@ -3,55 +3,69 @@ import { useState } from "react";
 export default function FeatureMatrixSelector({ featureValues, features, onChangeFeatureMatrix }) {
   const [ featureMatrix, setFeatureMatrix ] = useState({});
 
-  function onChangeFeatOrValue(e) {
+  function onChangeFeatOrValue(e, prevElemValue) {
+    const changedField = e.target.name;
+    
     const parent = e.target.parentElement;
     let featureSelector = parent.querySelector("[name=feature]");
     let featValueSelector = parent.querySelector("[name=feature-value]");
+
     const feature = featureSelector.value;
     const featureValue = featValueSelector.value;
+
+    const isNewRule = parent.dataset.newRule === "true";
     
-    if (feature && featureValue) {
+    if (!isNewRule && (feature === "" || featureValue === "")) { // setting an old rule to a blank
+      const {[prevElemValue]: _, ...newFeatMatrix} = {...featureMatrix};
+      console.log("a")
+      setFeatureMatrix(newFeatMatrix);
+    } else if ( // finished creating a new rule OR it's an old one and feature value has changed
+      (isNewRule && feature && featureValue) ||
+      (!isNewRule && changedField === "feature-value")
+    ) {
       const newFeatMatrix = {...featureMatrix};
       newFeatMatrix[feature] = featureValue;
       setFeatureMatrix(newFeatMatrix);
 
-      featureSelector.value = "";
-      featValueSelector.value = "";
+      if (isNewRule) {
+        featureSelector.value = "";
+        featValueSelector.value = "";
+      }
+    } else if (!isNewRule && changedField === "feature") { // changing the feature of an old rule
+      const {[prevElemValue]: _, ...newFeatMatrix} = {...featureMatrix};
+      newFeatMatrix[feature] = featureValue;
+      setFeatureMatrix(newFeatMatrix);
     }
   }
 
-  function singleRuleSelector(initialFeature, initialFeatValue) {
-    const isBlank = !(initialFeature && initialFeatValue);
-    
-    return <li>
-      <div>
-        <select
-          name="feature-value"
-          onChange={ onChangeFeatOrValue }
-          value={ initialFeatValue }
-        >
-          <option value=""></option>
-          { featureValues.map(val => <option value={ val }>{ val }</option>) }
-        </select>
+  function singleRuleSelector(isNewRule, initialFeature, initialFeatValue) {
+    return <li data-new-rule={ isNewRule } key={ initialFeature }>
+      <select
+        name="feature-value"
+        onChange={ e => onChangeFeatOrValue(e, initialFeatValue) }
+        value={ initialFeatValue }
+      >
+        <option value=""></option>
+        { featureValues.map(val => <option value={ val }>{ val }</option>) }
+      </select>
 
-        <select
-          name="feature"
-          onChange={ onChangeFeatOrValue }
-          value={ initialFeature }
-        >
-          <option value=""></option>
-          { features.toSorted().map(feat => <option value={ feat }>{ feat }</option>) }
-        </select>
-      </div>
+      <select
+        name="feature"
+        onChange={ e => onChangeFeatOrValue(e, initialFeature) }
+        value={ initialFeature }
+      >
+        <option value=""></option>
+        { features.toSorted().map(feat => <option value={ feat }>{ feat }</option>) }
+      </select>
     </li>;
   }
 
   return <ul>
     {
       Object.entries(featureMatrix)
-        .map(([feature, featureValue]) => singleRuleSelector(feature, featureValue))
+        .map(([feature, featureValue]) => singleRuleSelector(false, feature, featureValue))
     }
 
-    { singleRuleSelector() }
+    { singleRuleSelector(true) }
   </ul>;
 }
