a413b4f34203b43ecffe98699247400da1a7def6
fechan <work.fechan@gmail.com>
2024-03-07 19:16:19 -0800

    add a separate header that syncs side scroll

--- src/components/FeatureTable.js
+++ src/components/FeatureTable.js
@@ -1,5 +1,6 @@
-import { VariableSizeGrid as Grid } from 'react-window';
+import { VariableSizeGrid as Grid, FixedSizeList } from 'react-window';
 import AutoSizer from "react-virtualized-auto-sizer";
+import { useRef } from 'react';
 
 export default function({ features, inventory, initialInventory, ruleTransformation }) {
   let segments = inventory?.segments ?? [];
@@ -15,7 +16,7 @@ export default function({ features, inventory, initialInventory, ruleTransformat
 
     if (rowIndex === 0 && columnIndex > 0) {
       return <div style={ style } className="border-b p-2 flex items-end justify-center">
-        <span>{ features[columnIndex - 1].replace(/([A-Z])/g, ' $1').toLowerCase() }</span>
+        <span>x { features[columnIndex - 1].replace(/([A-Z])/g, ' $1').toLowerCase() }</span>
       </div>
     }
 
@@ -43,25 +44,55 @@ export default function({ features, inventory, initialInventory, ruleTransformat
     }
   }
 
+  function FeatureColHeader({ index, style }) {
+    return <div>
+        <span style={ style }>{ features[index].replace(/([A-Z])/g, ' $1').toLowerCase() }</span>
+    </div>
+  }
+
   const rowHeight = (row) => row === 0 ? 45*1.75 : 45;
   const columnWidth = (col) => col === 0 ? 100*1.5 : 100;
 
+  const featureHeaderRef = useRef(null);
+
   return (
-    <div style={{ flex: '1 1 auto' }}>
-    <AutoSizer>
-      {({ height, width }) => (
-        <Grid
-          width={ width }
-          height={ height }
-          columnCount={ features.length + 1 }
-          rowCount={ segments.length + 1 }
-          rowHeight={ rowHeight }
-          columnWidth={ columnWidth }
-        >
-          { Cell }
-        </Grid>
-      )}
-    </AutoSizer>
+    <div className="flex flex-col" style={{ flex: '1 1 auto' }}>
+      <div>
+        <AutoSizer disableHeight>
+          {({ width }) => (
+            <FixedSizeList
+              ref={featureHeaderRef}
+              width={ width }
+              height={ 45*1.75 }
+              itemCount={ features.length }
+              itemSize ={ 100 }
+              layout="horizontal"
+            >
+              { FeatureColHeader }
+            </FixedSizeList>
+          )}
+        </AutoSizer>
+      </div>
+      <div style={{ flex: '1 1 auto' }}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <Grid
+              width={ width }
+              height={ height }
+              columnCount={ features.length + 1 }
+              rowCount={ segments.length + 1 }
+              rowHeight={ rowHeight }
+              columnWidth={ columnWidth }
+              onScroll={({ scrollLeft, scrollTop }) => {
+                if (featureHeaderRef.current)
+                  featureHeaderRef.current.scrollTo(scrollLeft);
+              }}
+            >
+              { Cell }
+            </Grid>
+          )}
+        </AutoSizer>
+      </div>
     </div>
   );
 }
